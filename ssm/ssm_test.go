f876e3e9d36c7946178a8d999667fd7b04cd98a7
Luc Yriarte <lyriarte@gmail.com>
2019-01-04 14:44:05 +0100

    Agent Deserialize / Serialize testing

---
+++ ssm/ssm_test.go
@@ -0,0 +1,45 @@
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+// Copyright Luc Yriarte <luc.yriarte@thingagora.org> 2018 
+// License: Apache-2.0
+//
+// Signing State Machines tests
+//
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+package main
+
+import (
+	"testing"
+	"fmt"
+)
+
+
+//
+// Agent
+//
+
+func TestAgent(test *testing.T) {
+	fmt.Println("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- Agent")
+
+	var err error
+	var agent Agent
+
+	fmt.Println("---- ---- ---- ---- Deserialize")
+	strAgent := "{\"name\": \"John Doe\", \"pub\": \"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxR0XhIzP0S9WTx7giz0iXqMhuwrjiai7GX8esPKuKMKQuGej5xTpKrfAf6/RtVRNPV3PQy92NqGXk+35nQVnGJU/GEpq86SnRrWWxVSqQR5Nh87DxbR3eoAwcKLFymsixJoWvpm/DU5Ut+Iuqy4Zla2zM5gS62/xlv03VJWVBPFN99pBybPWw0WnRbpnGFIpgDtyMjaE4U48Lmq8wesQ6c2RSXSE/HC76DOhmNKAbgkBnpMxvgW1AGUCJfB4KfutOkLb0OOHIRUeJv+FySwIeXyMh2o3xUQCHWKxSN3Rawg1aJBy2wj1jR9yUAwraLIUzguTaLDUvVH/4eKRGSryzwIDAQAB-----END PUBLIC KEY-----\"}"
+	err = agent.Deserialize([]byte(strAgent))
+	if err != nil {
+		test.Fatal(err)
+	}
+	
+	fmt.Println("---- ---- ---- ---- Serialize")
+	bytesAgent, err := agent.Serialize()
+	if err != nil {
+		test.Fatal(err)
+	}
+	if bytesAgent == nil {
+		test.Fatal("bytesAgent")
+	}
+	
+	fmt.Println("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ")
+}
+
