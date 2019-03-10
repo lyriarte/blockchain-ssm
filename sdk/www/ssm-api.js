ff195a2af2ba5be9e4511bd41c558af344610662
Luc Yriarte <lyriarte@gmail.com>
2019-03-10 12:59:22 +0100

    Implement user registration in www sdk

--- sdk/www/ssm-api.js
+++ sdk/www/ssm-api.js
@@ -1,3 +1,48 @@
+var JSE = new JSEncrypt({default_key_size: 2048});
+
+function ssmRegister(user, admin, adminKey) {
+	JSE.setPrivateKey(adminKey);
+	var userStr = JSON.stringify(user);
+	var signStr = JSE.sign(userStr, CryptoJS.SHA256, "sha256");
+	var hostCmd = {
+		cmd: "invoke",
+		fcn: "register",
+		args: [userStr, admin, signStr]
+	}
+
+	return hostCmd;
+}
+
+function ssmCreate(ssm, admin, adminKey) {
+	var hostCmd = {
+		cmd: "invoke",
+		fcn: "create",
+		args: [ssm, admin]
+	}
+
+	return hostCmd;
+}
+
+function ssmStart(session, admin, adminKey) {
+	var hostCmd = {
+		cmd: "invoke",
+		fcn: "start",
+		args: [session, admin]
+	}
+
+	return hostCmd;
+}
+
+function ssmPerform(action, context, user, userKey) {
+	var hostCmd = {
+		cmd: "invoke",
+		fcn: "perform",
+		args: [action, context, user]
+	}
+
+	return hostCmd;
+}
+
 function ssmQuery(fcn, id) {
 	var hostCmd = {
 		cmd: "query",
