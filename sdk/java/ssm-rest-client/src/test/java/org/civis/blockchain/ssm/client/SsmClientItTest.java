1352ee5ffbfb50d087e2d2714dda5076aa7359c8
Adrien Poupard <adrien.poupard@gmail.com>
2019-03-26 14:27:04 +0100

    Use network-it to run integration test

--- sdk/java/ssm-rest-client/src/test/java/org/civis/blockchain/ssm/client/SsmClientItTest.java
+++ sdk/java/ssm-rest-client/src/test/java/org/civis/blockchain/ssm/client/SsmClientItTest.java
@@ -16,9 +16,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SsmClientItTest {
 
-    private static Signer signerAdrien;
-    private static Signer signerChuck;
-    private static Signer signerSarah;
+    private static final String NETWORK = "bclan-it/";
+
+    private static String uuid;
+
+    private static Signer signerAdam;
+    private static Signer signerBob;
+    private static Signer signerSam;
+
+    private static Agent agentAdam;
+    private static Agent agentBob;
+    private static Agent agentSam;
+
+
     private static SsmClient client;
     private static String ssmName;
     private static String sessionName;
@@ -26,13 +36,21 @@ public class SsmClientItTest {
 
     @BeforeAll
     public static void init() throws Exception {
-        signerAdrien = Signer.loadFromFile("adrien");
-        signerChuck = Signer.loadFromFile("chuck");
-        signerSarah = Signer.loadFromFile("sarah");
+        signerAdam = Signer.loadFromFile("adam", NETWORK+"adam");
+        signerBob = Signer.loadFromFile("bob",NETWORK+"bob");
+        signerSam = Signer.loadFromFile("sam", NETWORK+"sam");
+
+        uuid = UUID.randomUUID().toString();
+
+        agentAdam = Agent.loadFromFile("adam", NETWORK+"adam");
+        agentBob = Agent.loadFromFile("bob-"+uuid, NETWORK+"bob");
+        agentSam = Agent.loadFromFile("sam-"+uuid, NETWORK+"sam");
+
+
         client = SsmClient.fromConfigFile("ssm-client.properties");
-        String uuid = UUID.randomUUID().toString();
+
         ssmName = "CarDealership-" + uuid;
-        Map<String, String> roles = ImmutableMap.of("chuck", "Buyer", "sarah", "Seller");
+        Map<String, String> roles = ImmutableMap.of("bob", "Buyer", "sam", "Seller");
         sessionName = "deal20181201-" + uuid;
         session = new Session(ssmName, sessionName, "Used car for 100 dollars.", roles);
     }
@@ -42,80 +60,59 @@ public class SsmClientItTest {
     public void getListAdmin() throws Exception {
         CompletableFuture<List<String>> agentRet = client.listAdmins();
         List<String> agentFormClient = agentRet.get();
-        assertThat(agentFormClient).contains("adrien");
+        assertThat(agentFormClient).contains("adam");
     }
 
-    @Test
-    @Order(6)
-    public void listAgent() throws Exception {
-        CompletableFuture<List<String>> agentRet = client.listAgent();
-        List<String> agentFormClient = agentRet.get();
-        assertThat(agentFormClient).contains("chuck", "sarah");
-    }
-
-    @Test
-    @Order(7)
-    public void listSsm() throws Exception {
-        CompletableFuture<List<String>> agentRet = client.listSsm();
-        List<String> agentFormClient = agentRet.get();
-        assertThat(agentFormClient).contains("Car dealership", "Loop", "Negociation");
-    }
-
-    @Test
-    @Order(8)
-    public void listSession() throws Exception {
-        CompletableFuture<List<String>> agentRet = client.listSession();
-        List<String> agentFormClient = agentRet.get();
-        assertThat(agentFormClient).contains("bobloops", "carsale20190301", "deal20181201", "tomloops");
-    }
 
     @Test
     @Order(10)
     public void getAdminUser() throws Exception {
-        Agent agent = Agent.loadFromFile("adrien");
-        CompletableFuture<Agent> agentRet = client.getAdmin("adrien");
+        CompletableFuture<Agent> agentRet = client.getAdmin("adam");
         Agent agentFormClient = agentRet.get();
-        assertThat(agentFormClient).isEqualTo(agent);
+        assertThat(agentFormClient).isEqualTo(agentAdam);
     }
 
     @Test
     @Order(20)
-    public void registerChuck() throws Exception {
-        Agent agent = Agent.loadFromFile("chuck");
-        CompletableFuture<String> transactionEvent = client.registerUser(signerAdrien, agent);
+    public void registerBob() throws Exception {
+        CompletableFuture<String> transactionEvent = client.registerUser(signerAdam, agentBob);
         String trans = transactionEvent.get();
 
-        assertThat(trans).isEqualTo("4852f7a3db59d6b1cf157c8a295162bb17c19ff6e1b8915c7f98efcb052ed878");
+        assertThat(trans).isNotNull();
 
     }
 
     @Test
     @Order(30)
-    public void getAgentChuck() throws Exception {
-        Agent agent = Agent.loadFromFile("chuck");
-        CompletableFuture<Agent> agentRet = client.getAgent("chuck");
+    public void getAgentBob() throws Exception {
+        CompletableFuture<Agent> agentRet = client.getAgent(agentBob.getName());
         Agent agentFormClient = agentRet.get();
-        assertThat(agentFormClient).isEqualTo(agent);
+        assertThat(agentFormClient).isEqualTo(agentBob);
     }
 
     @Test
     @Order(40)
-    public void registerSarah() throws Exception {
-        Agent agent = Agent.loadFromFile("sarah");
-        CompletableFuture<String> transactionEvent = client.registerUser(signerAdrien, agent);
+    public void registerSam() throws Exception {
+        CompletableFuture<String> transactionEvent = client.registerUser(signerAdam, agentSam);
         String trans = transactionEvent.get();
         assertThat(trans).isNotNull();
     }
 
     @Test
     @Order(50)
-    public void getAgentSarah() throws Exception {
-        Agent agent = Agent.loadFromFile("sarah");
-        CompletableFuture<Agent> agentRet = client.getAgent("sarah");
+    public void getAgentSam() throws Exception {
+        CompletableFuture<Agent> agentRet = client.getAgent(agentSam.getName());
         Agent agentFormClient = agentRet.get();
-        assertThat(agentFormClient).isEqualTo(agent);
+        assertThat(agentFormClient).isEqualTo(agentSam);
     }
 
+    @Test
+    @Order(55)
+    public void listAgent() throws Exception {
+        CompletableFuture<List<String>> agentRet = client.listAgent();
+        List<String> agentFormClient = agentRet.get();
+        assertThat(agentFormClient).contains(agentBob.getName(), agentSam.getName());
+    }
 
     @Test
     @Order(60)
@@ -124,7 +121,7 @@ public class SsmClientItTest {
         Ssm.Transition buy = new Ssm.Transition(1, 2, "Buyer", "Buy");
         Ssm ssm = new Ssm(ssmName, Lists.newArrayList(sell, buy));
 
-        CompletableFuture<String> transactionEvent = client.create(signerAdrien, ssm);
+        CompletableFuture<String> transactionEvent = client.create(signerAdam, ssm);
         String trans = transactionEvent.get();
 
         assertThat(trans).isNotNull();
@@ -133,7 +130,6 @@ public class SsmClientItTest {
     @Test
     @Order(70)
     public void getSsm() throws Exception {
-
         CompletableFuture<Ssm> ssmReq = client.getSsm(ssmName);
         Ssm ssm = ssmReq.get();
         assertThat(ssm).isNotNull();
@@ -143,10 +139,10 @@ public class SsmClientItTest {
     @Test
     @Order(80)
     public void start() throws Exception {
-        Map<String, String> roles = ImmutableMap.of("chuck", "Buyer", "sarah", "Seller");
+        Map<String, String> roles = ImmutableMap.of("bob", "Buyer", "sam", "Seller");
         Session session = new Session(ssmName, sessionName, "Used car for 100 dollars.", roles);
 
-        CompletableFuture<String> transactionEvent = client.start(signerAdrien, session);
+        CompletableFuture<String> transactionEvent = client.start(signerAdam, session);
         String trans = transactionEvent.get();
 
         assertThat(trans).isNotNull();
@@ -175,7 +171,7 @@ public class SsmClientItTest {
     @Order(100)
     public void performSell() throws Exception {
         Context context = new Context(sessionName, "100 dollars 1978 Camaro", 0);
-        CompletableFuture<String> transactionEvent = client.perform(signerSarah, "Sell", context);
+        CompletableFuture<String> transactionEvent = client.perform(signerSam, "Sell", context);
         String trans = transactionEvent.get();
         assertThat(trans).isNotNull();
     }
@@ -195,7 +191,7 @@ public class SsmClientItTest {
     @Order(120)
     public void performBuy() throws Exception {
         Context context = new Context(sessionName, "Deal !", 1);
-        CompletableFuture<String> transactionEvent = client.perform(signerChuck, "Buy", context);
+        CompletableFuture<String> transactionEvent = client.perform(signerBob, "Buy", context);
         String trans = transactionEvent.get();
         assertThat(trans).isNotNull();
     }
@@ -209,5 +205,22 @@ public class SsmClientItTest {
         SessionState stateExcpected = new SessionState(ssmName, sessionName, "Deal !", session.getRoles(), buy, 2, 2);
         assertThat(state).isEqualTo(stateExcpected);
     }
+
+    @Test
+    @Order(140)
+    public void listSsm() throws Exception {
+        CompletableFuture<List<String>> agentRet = client.listSsm();
+        List<String> agentFormClient = agentRet.get();
+        assertThat(agentFormClient).contains(ssmName);
+    }
+
+    @Test
+    @Order(150)
+    public void listSession() throws Exception {
+        CompletableFuture<List<String>> agentRet = client.listSession();
+        List<String> agentFormClient = agentRet.get();
+        assertThat(agentFormClient).contains(sessionName);
+    }
+
 
 }
